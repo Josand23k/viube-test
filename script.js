0dde9017da58b8787ec6f00667bc4fc8908d1add
Jose Sandoval <teotladventures@gmail.com>
2023-01-10 21:00:13 +0100

    fixing responsive navbar

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+/* -- MENU TOGGLE -- */
+const primaryNav = document.querySelector(".primary-nav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+});
+
 // -- slide --  //
 let slideIndex = 1;
 showSlides(slideIndex);
